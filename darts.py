bb1c64f50bbc9109791b23ff3e231af1ba5b055b
Ayden Chance <achance6@jhu.edu>
2023-10-25 14:18:41 -0400

    fixed redundancy

--- darts.py
+++ darts.py
@@ -2,10 +2,10 @@ import math
 
 def score(x, y):
     dist = math.sqrt(x ** 2 + y ** 2)
-    if -1 <= dist <= 1 and -1 <= dist <= 1:
+    if -1 <= dist <= 1:
         return 10
-    if -5 <= dist <= 5 and -5 <= dist <= 5:
+    if -5 <= dist <= 5:
         return 5
-    if -10 <= dist <= 10 or -10 <= dist <= 10:
+    if -10 <= dist <= 10:
         return 1
     return 0
