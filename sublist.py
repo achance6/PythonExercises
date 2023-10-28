c80cb21e63a1db31b6bbd41d69851be080ae1a16
Ayden Chance <achance6@jhu.edu>
2023-10-28 18:37:24 -0400

    minor edit

--- sublist.py
+++ sublist.py
@@ -3,13 +3,8 @@ SUPERLIST = 1
 EQUAL = 2
 UNEQUAL = 3
 
-
 def sublist(list_one: list, list_two: list) -> int:
-
-    if len(list_one) == len(list_two):
-        for index in range(len(list_one)):
-            if (list_one[index] != list_two[index]):
-                return UNEQUAL
+    if list_one == list_two:
         return EQUAL
     if len(list_one) > len(list_two):
         for i in range(len(list_one) - len(list_two) + 1):
@@ -20,5 +15,3 @@ def sublist(list_one: list, list_two: list) -> int:
             if list_two[i:i + len(list_one)] == list_one:
                 return SUBLIST
     return UNEQUAL
-
-print(sublist([2, 3], [1, 2, 3]))
