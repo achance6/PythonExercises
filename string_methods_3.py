b54c87b9520ef692eb7e773b3f39a05708987722
Ayden Chance <achance6@jhu.edu>
2023-11-01 11:14:02 -0400

    handles word problems with chained plus and minus

--- string_methods_3.py
+++ string_methods_3.py
@@ -9,12 +9,13 @@ def answer(question: str) -> int:
     if len(tokens) == 1 and tokens[0].lstrip('-').isdigit():
         return int(tokens[0])
     num1 = int(tokens.pop())
-    op = tokens.pop()
     while len(tokens) > 0:
+        op = tokens.pop()
+        num2 = int(tokens.pop())
         if op == 'plus':
-            num1 += int(tokens.pop())
+            num1 += num2
         elif op == 'minus':
-            num1 -= int(tokens.pop())
+            num1 -= num2
     return num1
         
-print(answer('What is 5 plus 5?'))
+print(answer('What is 5 plus 5 plus 5 minus 3?'))
