47ecff522ad3ff9e1fc1413ebd3fcc538bfa9cce
Ayden Chance <achance6@jhu.edu>
2023-11-09 15:14:16 -0500

    improved bob

--- bob.py
+++ bob.py
@@ -1,13 +1,18 @@
 def response(hey_bob: str) -> str:
     """Determines what bob will say in response to a given phrase"""
     hey_bob = hey_bob.rstrip()
-    if hey_bob.isupper() and hey_bob.endswith('?'):
-        return "Calm down, I know what I'm doing!"
-    if hey_bob.endswith('?'): 
-        return 'Sure.'
-    if hey_bob.isupper():
-        return 'Whoa, chill out!'
-    if hey_bob.isspace() or len(hey_bob) == 0:
+    is_shout = hey_bob.isupper()
+    is_question = hey_bob.endswith('?')
+    is_empty = hey_bob.isspace() or (len(hey_bob) == 0)
+
+    if is_empty:
         return 'Fine. Be that way!'
+    if is_shout and is_question:
+        return "Calm down, I know what I'm doing!"
+    if is_question: 
+        return 'Sure.'
+    if is_shout:
+        return 'Whoa, chill out!'
     return 'Whatever.'
     
+print(response("Tom-ay-to, tom-aaaah-to."))
