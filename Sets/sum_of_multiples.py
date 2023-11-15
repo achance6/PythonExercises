607fde11f745a441314534c659bbba7d26128479
Ayden Chance <achance6@jhu.edu>
2023-11-15 13:05:37 -0500

    set exercise added

--- Sets/sum_of_multiples.py
+++ Sets/sum_of_multiples.py
@@ -1,15 +1,12 @@
 def sum_of_multiples(limit: int, multiples: list[int]) -> int:
     union = set()
     for multiple in multiples:
-        union | get_multiples(multiple, limit)
+        union = union | get_multiples(multiple, limit)
     return sum(union)
 
 def get_multiples(num: int, limit: int) -> set[int]:
-    multiples = set()
-    i = 1
-    while (i * num) < limit:
-        multiples.add(i * num)
-        i += 1
-    return multiples
+    return {i for i in range(num, limit, num)} if num > 0 else set()
 
-print(get_multiples(5, 15))
+# print(get_multiples(5, 15))
+# print(sum_of_multiples(20, [3, 5]))
+print(sum_of_multiples(1, [0]))
