20ef022d1b31bb82202e9719ae9f4995f91c658e
Ayden Chance <achance6@jhu.edu>
2023-11-20 14:09:38 -0500

    Completed exercise and added comments

--- Unpacking/unpacking_3.py
+++ Unpacking/unpacking_3.py
@@ -1,34 +1,33 @@
-def transpose(lines: str) -> str:
-    lines_list = lines.split('\n')
-    max_len = len(max(lines_list, key = len))
-    for i, line in enumerate(lines_list):
-        line_len = len(line)
-        if line_len < max_len:
-            lines_list[i] += ' ' * (max_len - line_len)
+import itertools
 
-    zipped = zip(lines_list)
+def transpose(lines: str) -> str:
+    """Transposes strings separated by newlines"""
+    lines_list = lines.split('\n')
+    zipped = itertools.zip_longest(*lines_list, fillvalue='@')
     res = ''
+
     for line in zipped:
-        res += tuple_to_string(line)
+        # Only padding to the left with @'s to be replaced with spaces
+        # @'s are only used to fill when the inputs are mismatched
+        res += tuple_to_string(line).rstrip('@').replace('@', ' ')
         res += '\n'
-    res = res[:-1]
-    print(res)
+
+    # remove final newline
+    res = res[:-1] 
     return res
 
 def tuple_to_string(tup: tuple[str, ...]) -> str:
+    """Concatenates all strings in a tuples to a string"""
     res = ''
     for letter in tup:
         res += letter
-    return res.rstrip()
+    return res
 
+# transpose('\n'.join(['A1']))
 # transpose('\n'.join(['A1', 'B2', 'C3']))
 # transpose('\n'.join(['ABC', 'DEF']))
 # transpose('\n'.join(['ABC', 'DE']))
 # transpose('\n'.join(['The fourth line.', 'The fifth line.']))
-# 'TT\nhh\nee\n  \nff\noi\nuf\nrt\nth\nh \n l\nli\nin\nne\ne.\n. ' 
-# 'TT\nhh\nee\n  \nff\noi\nuf\nrt\nth\nh \n l\nli\nin\nne\ne.\n.'
+# transpose('\n'.join(['The first line.', 'The second line.']))
 # transpose('\n'.join(["The longest line.", "A long line.", "A longer line.", "A line."]))
 # transpose('\n'.join(["Single line."]))
-
-# 'S\ni\nn\ng\nl\ne\n\nl\ni\nn\ne\n.'
-# 'S\ni\nn\ng\nl\ne\n \nl\ni\nn\ne\n.'
