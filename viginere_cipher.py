6013188c428d8abbedd3732492cc3d2452179051
Ayden Chance <achance6@jhu.edu>
2023-11-30 16:35:42 -0500

    added type hint

--- viginere_cipher.py
+++ viginere_cipher.py
@@ -2,7 +2,7 @@ from itertools import cycle
 
 ALP_SIZE = 26 # size of alphabet used for modulo
 
-def viginere_cipher(message: str, key: str, encrypt = True) -> str:
+def viginere_cipher(message: str, key: str, encrypt: bool = True) -> str:
     """Encrypts or decrypts a given message using a the viginere cipher with
     a given key.
     :message: message to encrypt/decrypt
@@ -19,7 +19,7 @@ def viginere_cipher(message: str, key: str, encrypt = True) -> str:
                   if letter.isalpha() else letter)
     return trans
 
-def v_cipher_letter(letter: str, key_letter: str, encrypt = True) -> str:
+def v_cipher_letter(letter: str, key_letter: str, encrypt: bool = True) -> str:
     """Returns the encrypted or decrypted letter using viginere cipher.
     :letter: letter to encrypt/decrypt
     :key_letter: key letter to use
