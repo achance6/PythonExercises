153f36e2f8b3c7897d5e383b52a5b350b630d4ab
Ayden Chance <achance6@jhu.edu>
2023-11-30 16:37:04 -0500

    commented all driver code

--- AES.py
+++ AES.py
@@ -1,6 +1,7 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 import base64
+import time
 
 def encrypt(data: bytes, key: bytes) -> tuple[bytes, bytes, bytes]:
     cipher = AES.new(key, AES.MODE_EAX)
@@ -13,11 +14,28 @@ def decrypt(data: bytes, key: bytes, tag: bytes, nonce: bytes) -> bytes:
     cipher = AES.new(key, AES.MODE_EAX, nonce)
     return cipher.decrypt_and_verify(data, tag)
 
-key = get_random_bytes(16)
-message = 'Unsecured Message'
-ciphertext, tag, nonce = encrypt(message.encode(), key)
 
-encoded = base64.b64encode(ciphertext)
+# with open('bigbook.txt', 'rb') as file_in:
+#     message = file_in.read()
+# start_time = time.time()
 
-print(encoded)
-print(decrypt(base64.b64decode(encoded), key, tag, nonce).decode())
+# message = b"Unsecured message"
+
+
+# key_len = 16
+# key = get_random_bytes(key_len)
+# ciphertext, tag, nonce = encrypt(message, key)
+# encoded = base64.b64encode(ciphertext)
+# plaintext = decrypt(base64.b64decode(encoded), key, tag, nonce)
+
+# print(f'Original message is {message.decode()}')
+# print(f'Encoded message is {encoded.decode()}')
+# print(f'Decoded message is {plaintext.decode()}')
+
+# encrypt_decrypt_time = time.time() - start_time
+
+# with open('out.txt', 'wb') as file_out:
+#     file_out.write(plaintext)
+
+# with open('log.txt', 'a') as log:
+#     log.write(f'key length of {key_len}:{encrypt_decrypt_time * 1000:.2f}ms\n')
